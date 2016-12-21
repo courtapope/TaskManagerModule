ec61e202346cbf3a184fd9d25ff404961dc7de49
courtney <courtney@FE-DESKTOP1>
2016-12-21 11:30:35 -0500

    Update Edit.ascx to include more labels, textboxes, dropdownlists, and buttons.

--- Edit.ascx
+++ Edit.ascx
@@ -2,3 +2,27 @@
 <%@ Register TagPrefix="dnndev" TagName="label" Src="~/controls/LabelControl.ascx" %>
 
 <dnndev:label ID="lblName" runat="server" Text="Name" HelpText="This is my help text" />
+<asp:TextBox ID="txtName" runat="server" />
+<br />
+
+<dnndev:label ID="lblDescription" runat="server" Text="Description" HelpText="Enter the description for your task" />
+<asp:TextBox ID="txtDescription" runat="server" TextMode="MultiLine" Rows="5" Columns="20" />
+<br />
+
+<dnndev:label ID="lblAssignedUser" runat="server" Text="AssignedUsed" HelpText="Choose a user for the task" />
+<asp:DropDownList ID="ddlAssignedUser" runat="server" />
+<br />
+
+<dnndev:label ID="lblTargetCompletionDate" runat="server" Text="Target Completion Date" HelpText="When should the task be completed" />
+<asp:TextBox ID="txtTargetCompletionDate" runat="server" />
+<br />
+
+<dnndev:label ID="lblCompletionDate" runat="server" Text="Actual Completion Date" HelpText="When did the task get completed" />
+<asp:TextBox ID="txtCompletionDate" runat="server" />
+<br />
+
+<asp:LinkButton ID="btnSubmit" runat="server" Text="Save Task" />
+&nbsp
+
+<asp:LinkButton ID="btnCancel" runat="server" Text="Cancel" />
+<br />
--- Edit.ascx.designer.cs
+++ Edit.ascx.designer.cs
@@ -20,5 +20,104 @@ namespace Website.DesktopModules.TaskManagerModule {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DotNetNuke.UI.UserControls.LabelControl lblName;
+        
+        /// <summary>
+        /// txtName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+        
+        /// <summary>
+        /// lblDescription control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DotNetNuke.UI.UserControls.LabelControl lblDescription;
+        
+        /// <summary>
+        /// txtDescription control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescription;
+        
+        /// <summary>
+        /// lblAssignedUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DotNetNuke.UI.UserControls.LabelControl lblAssignedUser;
+        
+        /// <summary>
+        /// ddlAssignedUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlAssignedUser;
+        
+        /// <summary>
+        /// lblTargetCompletionDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DotNetNuke.UI.UserControls.LabelControl lblTargetCompletionDate;
+        
+        /// <summary>
+        /// txtTargetCompletionDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTargetCompletionDate;
+        
+        /// <summary>
+        /// lblCompletionDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DotNetNuke.UI.UserControls.LabelControl lblCompletionDate;
+        
+        /// <summary>
+        /// txtCompletionDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCompletionDate;
+        
+        /// <summary>
+        /// btnSubmit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnSubmit;
+        
+        /// <summary>
+        /// btnCancel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnCancel;
     }
 }
